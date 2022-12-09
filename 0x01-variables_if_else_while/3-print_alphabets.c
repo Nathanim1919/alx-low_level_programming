87d0e21ba1bfa80b5c736739668fa33e18c4e2b4
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-09 12:03:41 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 	char a;
 	char b;
 
-	for(a = 'a'; a <= 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
 
-	for(b = 'A'; b <= 'Z'; b++)
+	for (b = 'A'; b <= 'Z'; b++)
 		putchar(b);
 	putchar('\n');
 	return (0);
