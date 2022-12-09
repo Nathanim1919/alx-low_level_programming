299ccd617ec208e3e2fed18bb6a54e1d108bb592
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-09 13:34:53 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,13 @@ int main(void)
 	char c;
 
 	for (n = '0'; n <= '9'; n++)
-		putchar(n);	
+	{
+		putchar(n);
+	}	
 	for (c = 'a'; c <= 'f'; c++)
+	{
 		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
