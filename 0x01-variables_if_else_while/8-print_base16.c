953553ecbf4c5c97316c2825fd334bc0fb1becb7
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-09 14:49:51 -0800

    code fixed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,16 +13,14 @@ int main(void)
 	c = 'a';
 	d = 0;
 
-	while 
-		(d < 10) {
-			putchar(d + '0');
-			d++;
-		}
-	while
-		(c <= 'f') {
-			putchar(c);
-			c++;
-		}
+	while (d < 10) {
+		putchar(d + '0');
+		d++;
+	}
+	while (c <= 'f') {
+		putchar(c);
+		c++;
+	}
 	putchar('\n');
 	return (0);
-}    
+}	
