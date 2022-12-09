a6c5aa73ddb9e78cc0e9d8c8fa5ba20d60db31be
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-09 14:28:19 -0800

    code fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- *
- * main- Entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -14,7 +13,7 @@ int main(void)
 	while (i <= 7)
 	{
 		j = i + 1;
-		while(j <= 8)
+		while (j <= 8)
 		{
 			k = j + 1;
 			while (k <= 9)
