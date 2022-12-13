fb72125d8e5a861e481aac8c5ed5efc28f8e489d
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-13 12:53:46 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alhabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int n = 1;
+	char c ='a';
+
+	while (n <= 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
