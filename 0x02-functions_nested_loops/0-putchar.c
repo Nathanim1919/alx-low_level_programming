c3645a9c8f8c710af904a4f36866379d3f44cbf3
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-13 07:41:17 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -6,6 +7,14 @@
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return 0;
 }
