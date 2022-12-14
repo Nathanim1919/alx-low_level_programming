d3e9e9fdeb290e9452af653b7387d0f9f8352cd9
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-14 15:54:19 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d = 0;
+	long int a =1;
+	long int b = a;
+	long int c = a + b;
+
+	while (c < 4000000)
+	{
+		if (c % 2 == 0)
+		{
+			d += c;
+		}
+		a = b;
+		b = c;
+		c = a + b'
+	}
+	printf("%d\n", d);
+	return (0);
+}
+		
