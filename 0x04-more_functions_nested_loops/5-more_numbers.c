cfa6a122e518aa830553ba51f674783f02cf0513
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-15 15:05:12 -0800

    code fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * more_numbers -  prints 10 times the numbers, from 0 to 14
  */
@@ -11,7 +12,7 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-		  	if (c > 9)
+			if (c > 9)
 			{
 				_putchar((c / 10) + '0');
 			}
