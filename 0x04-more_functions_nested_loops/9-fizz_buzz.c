cc64525e6199bb6885f0b50ed84eec79956d1153
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-15 14:45:23 -0800

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 != 0))
+		{
+			printf("Fizz");
+		}
+		else if ((n % 3 != 0) && (n % 5 == 0))
+		{
+			printf("Buzz");
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+
+		if (n < 100)
+		{
+			printf(" ");
+		}
+	}
+}
+
