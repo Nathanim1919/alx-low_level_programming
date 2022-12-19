7e6ae3d31b6b757e338ae059535d0c578bc1c150
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-19 13:32:02 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - reverse astring
+ * @s: astrign to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char str[1000];
+
+	i = 0;
+
+	while (*(s + 1) !+ 0)
+	{
+		str[i = *(s + i);
+		i++;
+	}
+
+	j = i - 1;
+	i = 0;
+
+	while (j >= 0)
+	{
+		*(s + j) = str[i];
+		j--;
+		i++;
+	}
+}
