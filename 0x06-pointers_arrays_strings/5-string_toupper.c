9f4bbf2362e36a741c88e0de17b97f4f4a0a8f38
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-22 04:53:46 -0800

    to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * *string_toupper - changes all lowercase to uppercase
- * @*n: string
+ * @n: string
  * Return: char
  */
 char *string_toupper(char *n)
@@ -10,7 +10,7 @@ char *string_toupper(char *n)
 
 	while (n[i] != '\0')
 	{
-		if(n[i] >= 97 && n[i] <= 122)
+		if (n[i] >= 97 && n[i] <= 122)
 		{
 			n[i] = n[i] - 32;
 		}
