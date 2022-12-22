41d89824ca8ab2cf7251215ab758c6dd4966e4b3
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-22 04:50:49 -0800

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *string_toupper - changes all lowercase to uppercase
+ * @*n: string
+ * Return: char
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if(n[i] >= 97 && n[i] <= 122)
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
+
