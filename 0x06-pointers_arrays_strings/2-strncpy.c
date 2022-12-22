4b4c318f2bfde67305f94a2b5d9a41bf8d82689d
Nathanim1919 <nathanim2tadele@gmail.com>
2022-12-22 03:36:02 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\n';
+	dest[i] = '\0';
 	return (dest);
 }
