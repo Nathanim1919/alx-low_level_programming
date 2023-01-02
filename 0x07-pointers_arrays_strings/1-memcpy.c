3c65e268442e673acd2b3c662f7e004c8d9f0886
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-02 10:19:44 -0800

    write a function that copies a memofry area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,17 +3,17 @@
  * *_memcpy - copies n bytes from memory area src to dest
  * @dest: memory area dest
  * @src: src memory area
- * @n: n bygtes to be copied
+ * @n: n bytes to be copied
  * Return: char dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n >= 0; i++)
+	for (i = 0; n > 0; i++)
 	{
 		dest[i] = src[i];
-		n -= 1;
+	i	n -= 1;
 	}
 	return (dest);
 }
