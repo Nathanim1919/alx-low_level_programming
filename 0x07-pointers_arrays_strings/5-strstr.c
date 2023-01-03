f72b359a439a54b4b15a1e48044591d0f7f2cf2d
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-03 09:26:22 -0800

    write a function that locates asubstring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strstr - a function that locates a substring
+ * @haystack:  a string
+ * @needle: a substring
+ * Return: return apointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (; haystack[0]; haystack++)
+	{
+		for (i = 0; haystack[i] == needle[i]; i++)
+			;
+
+		if (!(needle[i]));
+			return (haystack);
+	}
+	return (0)
+}
