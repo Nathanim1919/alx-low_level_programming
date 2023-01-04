f17184bbbe2b85b965644dcbbb9f9a30b9c3a716
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-04 14:20:35 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,14 +8,14 @@
  */
 int opration(int n, int c)
 {
-	if ( c% (n / c) == 0)
+	if (c % (n / c) == 0)
 	{
 		if (c * (n / c) == n)
 			return (c);
 		else
 			return (-1);
 	}
-	return (0 + opration(n,c + 1));
+	return (0 + opration(n, c + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number.
