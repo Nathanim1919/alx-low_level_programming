2b3940fee902b48666dffeef96004dc41bf09fac
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-04 02:08:15 -0800

    factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,14 +6,14 @@
  */
 int factorial(int n)
 {
-	if (n == 1)
+	if (n == 0)
 	{
 		return (1);
 	}
 
-	if (n < 1)
+	if (n < 0)
 	{
-		return (0);
+		return (-1);
 	}
 	return (n * factorial(n - 1));
 }
