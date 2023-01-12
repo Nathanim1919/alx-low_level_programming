be9652d5bfea585a82cbf720b9926813ba0d7077
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-12 14:24:22 -0800

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,40 +5,38 @@
  * @s1: first string
  * @s2: second string
  * @n: size of the memory that is gonna be allocated
+ * Return:pointer to s1, n beytes of s2:
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int j;
 	unsigned int i;
+	unsigned int count1;
+	unsigned int count2;
 
-	str = malloc(sizeof(char) * n);
+	count1 = 0;
+	count2 = 0;
+
+	while (s1[count1] != '\0')
+		count1++;
+	while (s2[count2] != '\0')
+		count2++;
+	if (n > count2)
+		n = count2;
+	str = malloc(sizeof(char) * (count1 + (n + 1)));
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	if ((s1 == NULL) && (s2 == NULL))
-	{
-		str[0] = '\0';
-	}
-	else if (s1 == NULL)
-	{
-		s1[0] = '\0';
-	}
-	else if (s2 == NULL)
-	{
-		s2[0] = '\0';
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = 0;
 	while (s1[i] != '\0')
-	{
 		str[i] = s1[i];
-	}
 	j = 0;
 	while (s2[j] != '\0')
-	{
 		str[i] = s2[j];
-	}
 	str[i] = '\0';
 	return (str);
 }
