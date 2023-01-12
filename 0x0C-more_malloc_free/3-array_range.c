104ee946531c90de5014f7a49d0078afe813f43d
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-12 15:23:32 -0800

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(int) * (max + 1));
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 	{
 		return (NULL);
