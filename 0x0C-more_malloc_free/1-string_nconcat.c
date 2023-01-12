b7ba6a5dead8245ec7c9fb9d726e181377eefdd5
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-12 10:24:57 -0800

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	str = malloc(sizeof(char) * n);
-
 	if (str == NULL)
+	{
 		return (NULL);
+	}
+	if ((s1 == NULL) && (s2 == NULL))
+	{
+		str[0] = '\0';
+	}
+	else if (s1 == NULL)
+	{
+		s1[0] = '\0';
+	}
+	else if (s2 == NULL)
+	{
+		s2[0] = '\0';
+	}
 	i = 0;
 	while (s1[i] != '\0')
 	{
@@ -26,5 +39,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[i] = s2[j];
 	}
+	str[i] = '\0';
 	return (str);
 }
