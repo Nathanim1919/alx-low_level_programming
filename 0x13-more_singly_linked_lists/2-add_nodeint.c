23a027a914375448c449cead918190b785079722
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-30 14:33:31 -0800

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,19 +9,11 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node;
-	const int dup;
 
 	node = malloc(sizeof(listint_t));
 
 	if (node  == NULL)
 		return (NULL);
-
-	dup = strdup(n);
-	if (dup == NULL)
-	{
-		free(node);
-		return (NULL);
-	}
 
 	node->n = n;
 	node->next = *head;
