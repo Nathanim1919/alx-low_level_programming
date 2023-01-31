37274dadd07d52b1c5ea58802067e5ff79bd5937
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-30 16:38:21 -0800

    free listint

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: point to the head of the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *last;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (head)
+	{
+		last = head->next;
+
+		last = (*head)->next;
+		free(*head);
+		*head = last;
+	}
+	*head = NULL;
+}
