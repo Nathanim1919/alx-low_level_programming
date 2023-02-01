03850adaaa5731b84660d8f1f9ed6e65d4f4ac78
Nathanim1919 <nathanim2tadele@gmail.com>
2023-01-31 17:29:13 -0800

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				return (NULL);
 			current->next = newnode;
 			newnode->next = current->next->next;
+			newnode->n = n;
 			return (newnode);
 		}
 		current = current->next;
