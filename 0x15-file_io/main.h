f95d7543f474e6628bba9d8763ec0d5aeb54bd75
Nathanim1919 <nathanim2tadele@gmail.com>
2023-02-09 11:13:48 -0800

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#define BUF_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void close_file(int FD);
+int _strlen(char *str);
+
 #endif
