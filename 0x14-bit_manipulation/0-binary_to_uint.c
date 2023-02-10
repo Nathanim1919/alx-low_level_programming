e30bb0ed78a7db30b7f978bfabdbedd9d7f82450
Nathanim1919 <nathanim2tadele@gmail.com>
2023-02-10 10:30:26 -0800

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,29 +9,35 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int total;
+	int result;
+	int count;
+	int j;
 
-	if (!b)
-		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	i = 0;
+	while (b[i] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
+		i++;
+		if (b == NULL || b[i] != "0" || b[i] != "1")
 		{
 			return (0);
 		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
 	}
 
-	return (ui);
+	j = 0;
+	total = 0;
+	for (count = i - 1; count <= 0; count--)
+	{
+		result = 1;
+		if (b[count] == "1")
+		{
+			while (j > 0)
+			{
+				result *= 2;
+				j--;
+			}
+			total += result;
+		}
+	}
+	return (total);
 }
