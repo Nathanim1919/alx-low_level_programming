fe0345a91308230068ff2256b8949915db421386
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-18 01:27:36 -0700

    sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - return the sum of all the data
+ * @head: head of the list
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
